bf14b941428361fbd38650dcb7827854da4623bd
thinkgo <thinkgo@aliyun.com>
2024-10-21 08:55:19 +0800

    fix: remove sys-resource level

---
+++ admin/migrations/20241021005356_sys-resource-remove-level.sql
@@ -0,0 +1,2 @@
+-- Modify "sys_resource" table
+ALTER TABLE `sys_resource` DROP COLUMN `level`;
--- admin/schema/sys_resource.sql
+++ admin/schema/sys_resource.sql
@@ -3,7 +3,6 @@ CREATE TABLE
   `sys_resource` (
     `id` bigint (20) NOT NULL AUTO_INCREMENT COMMENT '资源id',
     `pid` bigint (20) NOT NULL DEFAULT '0' COMMENT '父级资源id',
-    `level` int (10) unsigned NOT NULL DEFAULT '0' COMMENT '层级',
     `tier` varchar(512) NOT NULL COMMENT '层级',
     `type` varchar(8) NOT NULL DEFAULT 'menu' COMMENT '资源类型,[menu:目录,carte:菜单,btn:按钮]',
     `name` varchar(64) NOT NULL DEFAULT '' COMMENT '资源代码(路由name)',
@@ -15,8 +14,8 @@ CREATE TABLE
     `component` varchar(255) NOT NULL DEFAULT '' COMMENT '组件',
     `remark` varchar(64) NOT NULL DEFAULT '' COMMENT '备注',
     `sort` int (10) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
-    `visible` char(1) NOT NULL DEFAULT '1' COMMENT '是否可见',
     `is_enabled` char(1) NOT NULL DEFAULT '1' COMMENT '是否使能',
+    `visible` char(1) NOT NULL DEFAULT '1' COMMENT '是否可见',
     `is_affix` char(1) NOT NULL DEFAULT '0' COMMENT '是否固定在tagsView栏',
     `is_cached` char(1) NOT NULL DEFAULT '0' COMMENT '是否缓存组件,对外链,内存无效',
     `is_iframe` char(1) NOT NULL DEFAULT '0' COMMENT '是否内嵌窗口,仅对hyperlink有效,hyperlink为空,必须为false',
